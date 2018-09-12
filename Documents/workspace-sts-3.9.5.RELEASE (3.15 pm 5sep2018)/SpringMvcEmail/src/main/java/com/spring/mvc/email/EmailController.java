d7c5d041d882a72eaa50a24a979fc871568b6687
harish <harish26verma@gmail.com>
2018-09-12 18:31:42 +0530

    with Error creating bean with name 'email Controller': Unsatisfied dependency expressed through field 'freemarkerConfig'; nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'freemarker.template.Configuration' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {@org.springframework.beans.factory.annotation.Autowired(required=true)}
    error

--- Documents/workspace-sts-3.9.5.RELEASE (3.15 pm 5sep2018)/SpringMvcEmail/src/main/java/com/spring/mvc/email/EmailController.java
+++ Documents/workspace-sts-3.9.5.RELEASE (3.15 pm 5sep2018)/SpringMvcEmail/src/main/java/com/spring/mvc/email/EmailController.java
@@ -12,12 +12,15 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,11 +28,26 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import freemarker.template.Configuration;
+import freemarker.template.Template;
 
 @Controller
 public class EmailController {
 
 	static String emailToRecipient, emailSubject, emailMessage;
+	static String Reservation_No = "";
+	static String Go_MMT_ID = "";
+	static String Guest_name = "";
+	static String Phone_no = "";
+	static String Room_type = "";
+	static String Meal_plan = "";
+	static String No_of_rooms = "";
+	static String No_of_persons = "";
+	static String Arrival_Date = "";
+	static String Departure_Date = "";
+	static String No_of_nights = "";
+	static String Costs_per_night_per_room = "";
+	static String Hotel_taxes_to_be_collected_from_guest = "";
+	static String Billing_Type = "";
 
 	static String emailFromRecipient = "harish26verma@gmail.com";
 	static ModelAndView modelViewObj;
@@ -37,16 +55,65 @@ public class EmailController {
 	@Autowired
 	private JavaMailSender mailSenderObj;
 
+	
+	
+	@Autowired
+	Configuration freemarkerConfig;  
+
 	@RequestMapping(value = { "/", "emailForm" }, method = RequestMethod.GET)
-	public ModelAndView showEmailForm(ModelMap model) {
+	public ModelAndView showEmailForm(Model modeltemplate) {
+
+		AxisRoom roomList = new AxisRoom("0039001170", "NH7003094126197", "Pravin sahu", "919006633325",
+				"Standard Room", "CP = Room+Breakfast	", "1", "2", "10/9/2018", "11/9/2018", "1", "3099.00 + Tax",
+				"null", "Bill to company");
+
+		/*
+		 * AxisRoom roomList = new AxisRoom(Reservation_No, Go_MMT_ID, Guest_name,
+		 * Phone_no, Room_type, Meal_plan, No_of_rooms, No_of_persons, Arrival_Date,
+		 * Departure_Date, No_of_nights, Costs_per_night_per_room,
+		 * Hotel_taxes_to_be_collected_from_guest, Billing_Type);
+		 */
+		modeltemplate.addAttribute("Reservation_No", roomList.getReservation_No());
+		modeltemplate.addAttribute("Go_MMT_ID", roomList.getGo_MMT_ID());
+		modeltemplate.addAttribute("Guest_name", roomList.getGuest_name());
+		modeltemplate.addAttribute("Phone_no", roomList.getPhone_no());
+		modeltemplate.addAttribute("Room_type", roomList.getRoom_type());
+		modeltemplate.addAttribute("Meal_plan", roomList.getMeal_plan());
+		modeltemplate.addAttribute("No_of_rooms", roomList.getNo_of_rooms());
+		modeltemplate.addAttribute("No_of_persons", roomList.getNo_of_persons());
+		modeltemplate.addAttribute("Arrival_Date", roomList.getArrival_Date());
+		modeltemplate.addAttribute("Departure_Date", roomList.getDeparture_Date());
+		modeltemplate.addAttribute("No_of_nights", roomList.getNo_of_nights());
+		modeltemplate.addAttribute("Costs_per_night_per_room", roomList.getCosts_per_night_per_room().toString());
+		modeltemplate.addAttribute("Hotel_taxes_to_be_collected_from_guest",
+				roomList.getHotel_taxes_to_be_collected_from_guest().toString());
+		modeltemplate.addAttribute("Billing_Type", roomList.getBilling_Type());
+
 		modelViewObj = new ModelAndView("templateflt");
-		// code to list the data from AxisRooms
-		AxisRoomsDataController roomList=new AxisRoomsDataController();
-	 List<AxisRoom> AxisRoomsListArray = roomList.getAxisRooms();
-	 Map<String, Object> root = new HashMap<String, Object>();
-	    root.put( "AxisRoomsListArray", AxisRoomsListArray );
-	 	
 		return modelViewObj;
+	}
+
+	@RequestMapping(value = { "/", "addAxisHotel" }, method = RequestMethod.POST)
+	public ModelAndView addRoom(HttpServletRequest request, Model modeltemplate) {
+
+		Reservation_No = request.getParameter("Reservation_No");
+		Go_MMT_ID = request.getParameter("Go_MMT_ID");
+		Guest_name = request.getParameter("Guest_name");
+		Phone_no = request.getParameter("Phone_no");
+		Room_type = request.getParameter("Room_type");
+		Meal_plan = request.getParameter("Meal_plan");
+		No_of_rooms = request.getParameter("No_of_rooms");
+		No_of_persons = request.getParameter("No_of_persons");
+		Arrival_Date = request.getParameter("Arrival_Date");
+		Departure_Date = request.getParameter("Departure_Date");
+		No_of_nights = request.getParameter("No_of_nights");
+		Costs_per_night_per_room = request.getParameter("Costs_per_night_per_room");
+		Hotel_taxes_to_be_collected_from_guest = request.getParameter("Hotel_taxes_to_be_collected_from_guest");
+		Billing_Type = request.getParameter("Billing_Type");
+
+		modelViewObj = new ModelAndView("addAxisHotel");
+		return modelViewObj;
+
 	}
 
 	// This Method Is Used To Prepare The Email Message And Send It To The Client
@@ -70,6 +137,17 @@ public class EmailController {
 				mimeMsgHelperObj.setFrom(emailFromRecipient);
 				mimeMsgHelperObj.setText(emailMessage);
 				mimeMsgHelperObj.setSubject(emailSubject);
+
+				
+				
+				
+				
+				Map<String, Object> model = new HashMap();
+				model.put("emailUser", emailFromRecipient);
+
+				Template t = freemarkerConfig.getTemplate("templateflt.ftl");
+				String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+				mimeMsgHelperObj.setText(html, true);
 
 				// Determine If There Is An File Upload. If Yes, Attach It To The Client Email
 				if ((attachFileObj != null) && (attachFileObj.getSize() > 0) && (!attachFileObj.equals(""))) {
